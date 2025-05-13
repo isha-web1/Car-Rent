2923ab24a81eb1137d315d8ef0a4efaa073c6d27
isha-web1 <isha.web1@gmail.com>
2025-05-13 23:07:14 +0600

    service section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import ServicesSection from "./components/ServicesSection";
 
 export default function Home() {
   return (
     <div>
     
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ServicesSection/>
     </div>
   );
 }
